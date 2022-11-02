0009e685c880aca832ba216faefc5e19fac6d63c
Marius <62306974+ruauno@users.noreply.github.com>
2022-11-02 09:49:50 +0100

    Create ft_atoi.c

---
+++ ex03/ft_atoi.c
@@ -0,0 +1,35 @@
+int		ft_atoi(char *str)
+{
+	int c;
+	int s;
+	int res;
+
+	c = 0;
+	s = 1;
+	res = 0;
+	while ((str[c] >= '\t' && str[c] <= '\r') || str[c] == ' ')
+		c++;
+	while (str[c] == '+' || str[c] == '-')
+	{
+		if (str[c] == '-')
+			s *= -1;
+		c++;
+	}
+	while (str[c] >= '0' && str[c] <= '9')
+	{
+		res = (str[c] - '0') + (res * 10);
+		c++;
+	}
+	return (res * s);
+}
+/*
+#include <stdio.h>
+
+int		ft_atoi(char *str);
+
+int		main(void)
+{
+	printf("%d\n", ft_atoi("      	---+--+2147--+-ab567"));
+
+}
+*/
