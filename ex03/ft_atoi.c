954f6dbbd63e3ddf64c53c8419bdc555919d8526
Marius <62306974+ruauno@users.noreply.github.com>
2022-11-03 09:55:29 +0100

    Update ft_atoi.c

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -23,13 +23,36 @@ int		ft_atoi(char *str)
 	return (res * s);
 }
 /*
-#include <stdio.h>
-
-int		ft_atoi(char *str);
-
-int		main(void)
+# include <unistd.h>
+# include <stdio.h>
+int	main(void)
 {
-	printf("%d\n", ft_atoi("      	---+--+2147--+-ab567"));
 
+	char	*str1 = " ---+--+1234ab567";
+	printf("\"%s\": %d\n", str1, ft_atoi(str1));
+
+	char	*str2 = " ---+- -+1234ab567";
+	printf("\"%s\": %d\n", str2, ft_atoi(str2));
+
+	char	*str3 = " \n---+-+1234ab567";
+	printf("\"%s\": %d\n", str3, ft_atoi(str3));
+
+	char	*str4 = " ---+--+a1234ab567";
+	printf("\"%s\": %d\n", str4, ft_atoi(str4));
+
+	char	*str5 = "  0";
+	printf("\"%s\": %d\n", str5, ft_atoi(str5));
+
+	char	*str6 = "2147483647";
+	printf("\"%s\": %d\n", str6, ft_atoi(str6));
+
+	char	*str7 = "-2147483648";
+	printf("\"%s\": %d\n", str7, ft_atoi(str7));
+
+	char	*str8 = "abc1234ab567";
+	printf("\"%s\": %d\n", str8, ft_atoi(str8));
+	printf("\n");
+
+	return (0);
 }
 */
