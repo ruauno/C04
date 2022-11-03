dfe16acabb8ac00a8160d154679ae5f9164da2a3
Marius <62306974+ruauno@users.noreply.github.com>
2022-11-03 09:28:26 +0100

    Update ft_putnbr.c

--- ex02/ft_putnbr.c
+++ ex02/ft_putnbr.c
@@ -1,41 +1,33 @@
 #include <unistd.h>
 
-void	ft_putchar(char a)
+void	ft_putchar(char c)
 {
-	write(1, &a, 1);
+	write(1, &c, 1);
 }
 
 void	ft_putnbr(int nb)
 {
 	if (nb == -2147483648)
-	{
-		ft_putchar('-');
-		ft_putchar('2');
-		nb = 147483648;
-	}
-	if (nb < 0)
-	{
-		ft_putchar('-');
-		nb *= -1;
-	}
-	if (nb < 10)
-	{
-		ft_putchar(nb + 48);
-		return ;
-	}
+		write(1, "-2147483648", 11);
 	else
-		ft_putnbr(nb / 10);
-	ft_putnbr(nb % 10);
+	{
+		if (nb < 0)
+		{
+			ft_putchar('-');
+			nb = -nb;
+		}
+		if (nb > 9)
+			ft_putnbr(nb / 10);
+		ft_putchar(nb % 10 + '0');
+	}
 }
 /*
 #include <stdio.h>
-
 void	ft_putnbr(int n);
-
 int		main(void)
 {
     printf(" (printed) should be 1");
-    ft_putnbr(1);
+    ft_putnbr(2147483647);
     printf("\n (printed) should be 0");
     ft_putnbr(0);
     printf("\n (printed) should be -1");
